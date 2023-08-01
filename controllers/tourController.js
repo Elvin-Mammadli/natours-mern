6c5aec54ee5404e93b3345eae10a031be712ea8a
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-08-01 22:50:04 +0400

    tour crud apis created

---
+++ controllers/tourController.js
@@ -0,0 +1,94 @@
+const Tour = require('../models/tourModels');
+
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find({});
+    res.status(200).json({
+      status: 'success',
+      data: tours,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+const getTourById = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: tour,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+const createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+const updateTour = async (req, res) => {
+  try {
+    const updateTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: updateTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+const deleteTour = async (req, res) => {
+  try {
+    const deleted = await Tour.findOneAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      message: `${deleted.name} has been deleted successfully!`,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+module.exports = {
+  getAllTours,
+  getTourById,
+  createTour,
+  updateTour,
+  deleteTour,
+};
