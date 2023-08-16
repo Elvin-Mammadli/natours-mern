767e55337b689cd8a98891fdb70d9dc1a618c6e0
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-08-16 20:05:36 +0400

    error handling implemented for unhandled routes

--- app.js
+++ app.js
@@ -19,8 +19,14 @@ app.use((req, res, next) => {
 });
 
 // Routes
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
