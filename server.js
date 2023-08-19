189fa77badd1a2019eb5f4711bbf97dcfe715110
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-08-19 14:37:20 +0400

    stop server when unhandled rejection - added

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose.connect(DB).then(() => console.log('DB connected.'));
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connected.'))
+  .catch((err) => console.log('ERROR'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Server started...');
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION!');
+  server.close(() => {
+    process.exit(1);
+  });
 });
