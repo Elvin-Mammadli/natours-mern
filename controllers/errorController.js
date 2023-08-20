3c2a09761ecae775c534129bd4f85675d8eda4f7
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-08-20 11:07:00 +0400

    node version error fixed & error bug fixed

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -56,10 +56,9 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
 
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === '11000') error = handleDuplicateFieldsDB(error);
-    if (error.code === 'ValidationError')
-      error = handleValidationErrorDB(error);
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === '11000') error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
