9bcb41bfd690138cf99eff088314b29e9e2e31e1
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-08-30 21:55:53 +0400

    geospatial data added to tour model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const Tour = require('../models/tourModels');
+const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
