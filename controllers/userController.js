a3bdc26e34bf878525a8ff0c143a5d91852dbac0
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-02 22:43:54 +0400

    factory function implemented to user delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -71,12 +72,7 @@ exports.updateUser = (req, res) => {
   });
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined yet',
-  });
-};
+exports.deleteUser = factory.deleteOne(User);
 
 exports.getUserById = (req, res) => {
   res.status(500).json({
