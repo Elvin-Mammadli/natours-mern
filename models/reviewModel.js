956ff26182feff8a53db28fae9fec134644d944e
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-05 22:55:23 +0400

    average rating function added

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = mongoose.Schema(
   {
@@ -51,6 +52,31 @@ reviewSchema.pre(/^find/, function (next) {
 
   next();
 });
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgnRating,
+  });
+}
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour)
+})
 
 const Review = mongoose.model('Review', reviewSchema);
 
