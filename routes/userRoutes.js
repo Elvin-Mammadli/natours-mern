c611d61646c1436d36333d85d6312f678abc7301
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-11 20:39:32 +0400

    added image upload with multer package

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,8 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.post('/logout', authController.logout);
+router.get('/logout', authController.logout);
+
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
@@ -15,7 +16,7 @@ router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUserById);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', userController.uploadUserPhoto, userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 // Restrict access to below routes only admin
