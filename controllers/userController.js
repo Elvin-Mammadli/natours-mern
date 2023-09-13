89980e9d84cdf7fab4853e3b9ea55184db2d1d15
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-13 20:36:53 +0400

    multiple image upload completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,17 +32,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   res.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${res.file.filename}`);
-};
+
+  next();
+});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
