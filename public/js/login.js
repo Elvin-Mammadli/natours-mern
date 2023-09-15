a381a657dc61f22b99913065a7c1df2414cd597f
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-15 20:04:49 +0400

    consoles cleared, compression package added

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
+const domain = process.env.NODE_ENV === 'production' ? '' : 'http://127.0.0.1:3000'
+
 export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: `${domain}/api/v1/users/login`,
       data: {
         email,
         password,
@@ -27,7 +29,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: `${domain}/api/v1/users/logout`,
     });
     if ((res.data.status = 'success')) location.reload(true);
   } catch (err) {
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,13 +2,15 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
+const domain = process.env.NODE_ENV === 'production' ? '' : 'http://127.0.0.1:3000'
+
 // type is either 'password' or 'data'
 export const updateSettings = async (data, type) => {
   try {
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        ? `${domain}/api/v1/users/updateMyPassword`
+        : `${domain}/api/v1/users/updateMe`;
 
     const res = await axios({
       method: 'PATCH',
