d9af2184abc3ed9d2b72cceeef9e337f2aabfb8a
Elvin Mammadli <mamedli.elvin@gmail.com>
2023-09-18 10:00:23 +0400

    created /my-tours route

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -44,4 +44,15 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   await Booking.create({ tour, user, price });
 
   res.redirect(res.originalUrl.split('?')[0]);
+
+  // const tour = session.client_reference_id;
+  // const user = (await User.findOne({ email: session.customer_email })).id;
+  // const price = session.display_items[0].amount / 100;
+  // await Booking.create({ tour, user, price });
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
